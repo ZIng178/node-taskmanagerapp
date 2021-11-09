3e00944c14c2578149336be6a4d7c1022d5800ba
Wangdi Tenzing <wangdi457@gmail.com>
2021-11-09 18:00:43 -0500

    Created routes for all the crud functionalites and tested the URLS in POSTMAN

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,6 +2,26 @@ const getAllTasks = (req, res) => {
   res.send("getting the items  ");
 };
 
+const createTask = (req, res) => {
+  res.json(req.body);
+};
+
+const getTask = (req, res) => {
+  res.json(req.params.id);
+};
+
+const updateTask = (req, res) => {
+  res.send("Updating tasks   ");
+};
+
+const deleteTask = (req, res) => {
+  res.send("Deleting tasks   ");
+};
+
 module.exports = {
   getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
 };
